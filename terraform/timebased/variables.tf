3e0b1717f2b0c93c55fdbe514dcf2fada442fa13
arafato <oliver.arafat@outlook.com>
2019-06-03 10:19:18 +0200

    refactoring

---
+++ terraform/timebased/variables.tf
@@ -0,0 +1,48 @@
+variable "cen_id" {
+  description = "The ID of your Cloud Enterprise Netork (CEN) instance."
+  default = "thisdoesnotexist"
+}
+
+variable "cen-scaler-policy" {
+  default = "cen-scaler-policy"
+}
+
+variable "cen-scaler-role" {
+  default = "cen-scaler-role"
+}
+
+variable "service_name" {
+  default     = "CENScalerService"
+}
+
+variable "service_description" {
+  default     = "Created by CEN-Scaler"
+}
+
+variable "function_name" {
+  default     = "scale"
+}
+
+variable "function_description" {
+  default     = "Created by CEN-Scaler"
+}
+
+variable "function_zip" {
+  description = "The path to the function's deployment package within the local filesystem. It is conflict with the oss_-prefixed options.."
+  default     = "fc.zip"
+}
+
+variable "function_memory_size" {
+  description = "Amount of memory in MB your Function can use at runtime. Defaults to 128. Limits to [128, 3072]."
+  default     = "128"
+}
+
+variable "function_runtime" {
+  description = "The Function Compute function runtime type."
+  default     = "nodejs8"
+}
+
+variable "function_handler" {
+  description = "The function entry point in your code."
+  default     = "index.handler"
+}
